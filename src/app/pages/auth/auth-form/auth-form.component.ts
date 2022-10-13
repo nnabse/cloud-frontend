820418a319f8cbb1cd24092be11108280b86cbcc
nnabse <nnabse@gmail.com>
2022-10-13 20:15:33 +0300

    sign up elements

--- src/app/pages/auth/auth-form/auth-form.component.ts
+++ src/app/pages/auth/auth-form/auth-form.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss'],
 })
-export class AuthFormComponent {}
+export class AuthFormComponent {
+  @Input() formFor: 'Sign In' | 'Sign Up' = 'Sign In';
+
+  labels = {
+    'Sign In': {
+      btnText: 'Sign In',
+      redirectBtnText: 'Sign Up',
+      redirectText: "Haven't account?",
+    },
+    'Sign Up': {
+      btnText: 'Sign Up',
+      redirectBtnText: 'Sign In',
+      redirectText: 'Already have an account?',
+    },
+  };
+}
